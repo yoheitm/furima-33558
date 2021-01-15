cf0bcf589a48732d340f7f36a7ed2622f168c90d
yoheitm <tmyoyy9617@gmail.com>
2021-01-15 19:14:00 +0900

    usersテーブルの設計

--- db/migrate/20210115090018_devise_create_users.rb
+++ db/migrate/20210115090018_devise_create_users.rb
@@ -6,6 +6,10 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.string :nickname,           null: false
+      t.string :first_name,         null: false
+      t.string :last_name,          null: false
+      t.integer :birthday,          null: false
 
       ## Recoverable
       t.string   :reset_password_token
