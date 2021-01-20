03c2a8b536102367773a12c65e29c146675280db
yoheitm <tmyoyy9617@gmail.com>
2021-01-20 14:44:50 +0900

    rubocopでの修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,10 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
+
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :email, :password, :first_name, :last_name, :first_name_kana, :last_name_kana, :birth_date ])
+    devise_parameter_sanitizer.permit(:sign_up,
+                                      keys: [:nickname, :email, :password, :first_name, :last_name, :first_name_kana,
+                                             :last_name_kana, :birth_date])
   end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
   factory :user do
-    nickname                   {Faker::Name.initials}
-    email                      {Faker::Internet.free_email}
-    password                   {Faker::Internet.password(min_length: 6)}
-    password_confirmation      {password}
-    first_name                 {Gimei.first.kanji}
-    last_name                  {Gimei.last.kanji}
-    first_name_kana            {Gimei.first.katakana}
-    last_name_kana             {Gimei.last.katakana}
-    birth_date                 {Faker::Date.birthday}
+    nickname                   { Faker::Name.initials }
+    email                      { Faker::Internet.free_email }
+    password                   { Faker::Internet.password(min_length: 6) }
+    password_confirmation      { password }
+    first_name                 { Gimei.first.kanji }
+    last_name                  { Gimei.last.kanji }
+    first_name_kana            { Gimei.first.katakana }
+    last_name_kana             { Gimei.last.katakana }
+    birth_date                 { Faker::Date.birthday }
   end
 end
