d899f3a91d1b4caf11f7ecd5faea7a73dae83d4b
yoheitm <tmyoyy9617@gmail.com>
2021-01-21 18:39:18 +0900

    ストロングパラメーターの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,11 +1,23 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: [:new]
   def index
   end
 
   def new
     @item = Item.new
-    unless user_signed_in?
-      redirect_to  new_user_registration_path
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
     end
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :info, :category_id, :state_id, :ship_base_id, :area_id, :ship_date_id, :price, :image).marge(user_id: current_user.id)
   end
 end
