d549e3c5d7f346193b787e1afe8d1ac5d5bb84db
yoheitm <tmyoyy9617@gmail.com>
2021-01-21 18:01:15 +0900

    ActiveHashを用いたモデルの編集

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,10 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
   has_one_attached :image
   belongs_to :user
+  belongs_to :area
+  belongs_to :category
+  belongs_to :ship_base
+  belongs_to :ship_date
+  belongs_to :sate
 end
