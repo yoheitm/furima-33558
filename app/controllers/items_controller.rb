775d3389b67593bb50f2db7b55205ba6a25d3302
yoheitm <tmyoyy9617@gmail.com>
2021-01-21 16:01:00 +0900

    newアクションの定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,11 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def new
+    @item = Item.new
+    unless user_signed_in?
+      redirect_to  new_user_registration_path
+    end
+  end
 end
