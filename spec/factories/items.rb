d5ee3c9627f5c0fe2c344b20f001e6127eb038f2
yoheitm <tmyoyy9617@gmail.com>
2021-01-22 11:55:17 +0900

    itemモデルの単体テストコードの実装

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :item do
-    
+    name                      { Faker::Lorem.words }
+    info                      { Faker::Lorem.characters }
+    category_id               { 2 }
+    state_id                  { 2 }
+    ship_base_id              { 2 }
+    area_id                   { 3 }
+    ship_date_id              { 2 }
+    price                     { 1000 }
+    association :user
   end
 end
